2c6dc1c6b95f6309afad16dc847c61009a489c82
kingBook <412126604@qq.com>
2020-05-12 16:04:25 +0800

    update 2020-05-12 16:04

---
+++ assets/scripts/Game.ts
@@ -0,0 +1,12 @@
+﻿import BaseGame from "../Framework/Core/BaseGame";
+
+const {ccclass, property} = cc._decorator;
+
+@ccclass
+export default class Game extends BaseGame{
+	
+	protected onLoad():void{
+		super.onLoad();
+	}
+	
+}
