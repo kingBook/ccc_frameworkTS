d4b0684bd83cafee88f340d1ad824fe379bc9d51
kingBook <412126604@qq.com>
2020-05-13 00:11:06 +0800

    update 2020-05-12 00:11

--- packages/helloEditor/main.js
+++ packages/helloEditor/main.js
@@ -1,17 +1,34 @@
 'use strict';
 
 module.exports = {
-  load () {
-    // 当 package 被正确加载的时候执行
-  },
+	load () {
+		// 当 package 被正确加载的时候执行
+	},
 
-  unload () {
-    // 当 package 被正确卸载的时候执行
-  },
+	unload () {
+		// 当 package 被正确卸载的时候执行
+	},
 
-  messages: {
-    'sayHello' () {
-      Editor.log('Hello World!');
-    }
-  },
+	messages: {
+		'say-hello' () {
+			Editor.log('Hello World!');
+		},
+		
+		'asset-db:assets-created'(){
+			Editor.log("asset-created");
+			Editor.assetdb.queryAssets('db://assets/**\/*', 'texture', function (err, results) {
+				results.forEach(function (result) {
+				// result.url
+				// result.path
+				// result.uuid
+				// result.type
+				// result.isSubAsset
+				
+				Editor.log(result.path);
+				
+				
+				});
+			});
+		}
+	},
 };
