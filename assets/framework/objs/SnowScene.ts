df31e233eeca85fe3eae0ee5d9e5130a8a1c07c7
kingBook <412126604@qq.com>
2020-07-21 15:21:54 +0800

    update 2020-07-21 15:21

---
+++ assets/framework/objs/SnowScene.ts
@@ -0,0 +1,74 @@
+﻿import BaseBehaviour from "../core/BaseBehaviour";
+import SnowSingle from "./SnowSingle";
+import NodeUtil from "../utils/NodeUtil";
+import Random from "../utils/Random";
+
+const{ccclass,property}=cc._decorator;
+/** 随机飘落的雪景 */
+@ccclass
+export default class SnowScene extends BaseBehaviour{
+	@property
+	private _snowPrefab:cc.Node=null;
+	@property({type:cc.Node,visible:true})
+	private _worldMinNode:cc.Node=null;
+	@property({type:cc.Node,visible:true})
+	private _worldMaxNode:cc.Node=null;
+	@property({visible:true})
+	private _snowTotal:number=100;
+	
+	@property({type:cc.Node,visible:true})
+	private set snowPrefab(value:cc.Node){
+		if(!value||value.getComponent(SnowSingle)){
+			this._snowPrefab=value;
+		}else{
+			cc.error("Snow Prefab必须绑定有SnowSingle组件");
+		}
+	}
+	
+	private _snowSingleList:SnowSingle[]=[];
+	
+	private get snowPrefab():cc.Node{
+		return this._snowPrefab;
+	}
+
+	protected onLoad():void{
+		super.onLoad();
+		this._snowPrefab.active=false;
+	}
+	
+	protected start():void{
+		super.start();
+		this._snowSingleList=this.createSnows();
+	}
+	
+	protected update():void{
+		for(let i=0,len=this._snowSingleList.length;i<len;i++){
+			let snowSingle=this._snowSingleList[i];
+			if(snowSingle.isOutBottom){
+				snowSingle.setPositionToTop(true,-Random.randomFloat(1));
+			}
+		}
+	}
+	
+	private createSnows():SnowSingle[]{
+		let list=[];
+		let minPos=NodeUtil.getLocalPositionV2UnderNode(this._worldMinNode,this.snowPrefab.parent);
+		let maxPos=NodeUtil.getLocalPositionV2UnderNode(this._worldMaxNode,this.snowPrefab.parent);
+		for(let i=0;i<this._snowTotal;i++){
+			let inst=cc.instantiate(this._snowPrefab);
+			inst.active=true;
+			inst.parent=this._snowPrefab.parent;
+			inst.setSiblingIndex(this._snowPrefab.getSiblingIndex());
+			
+			let snowSingle=inst.getComponent(SnowSingle);
+			snowSingle.setWorldRange(minPos,maxPos);
+			snowSingle.setPositionToRandom();
+			snowSingle.setScaleToRandom();
+			snowSingle.setVelocity(0,-Random.randomFloat(1));
+			list[i]=snowSingle;
+		}
+		return list;
+	}
+	
+	
+}
---
+++ assets/framework/objs/SnowSingle.ts
@@ -0,0 +1,65 @@
+﻿import BaseBehaviour from "../core/BaseBehaviour";
+import Random from "../utils/Random";
+
+const{ccclass,property}=cc._decorator;
+/** 雪（单粒子） */
+@ccclass
+export default class SnowSingle extends BaseBehaviour{
+	
+	private _worldMin:cc.Vec2=new cc.Vec2(-1e5,-1e5);
+	private _worldMax:cc.Vec2=new cc.Vec2(1e5,1e5);
+	private _gravity:cc.Vec2=new cc.Vec2(0,-0.00098);
+	private _minScale:number=0.3;
+	private _maxScale:number=1;
+	private _velocity:cc.Vec2=new cc.Vec2(0,0);
+	
+	public get isOutBottom():boolean{
+		return this.node.y-this.node.getBoundingBox().height*0.5<this._worldMin.y;
+	}
+	
+	protected update(dt:number):void{
+		super.update(dt);
+		
+		this._velocity.addSelf(this._gravity);
+		
+		let pos=this.node.getPosition();
+		pos.addSelf(this._velocity);
+		this.node.setPosition(pos);
+	}
+	
+	public setScaleRange(min:number,max:number):void{
+		this._minScale=min;
+		this._maxScale=max;
+	}
+	
+	public setScaleToRandom():void{
+		this.node.scale=Random.rangeFloat(this._minScale,this._maxScale);
+	}
+	
+	public setVelocity(x:number,y:number):void{
+		cc.Vec2.set(this._velocity,x,y);
+	}
+	
+	/** 设置World范围，以this.node.parent为坐标系原点 */
+	public setWorldRange(min:cc.Vec2,max:cc.Vec2):void{
+		this._worldMin.set(min);
+		this._worldMax.set(max);
+	}
+	
+	public setPositionToRandom():void{
+		let x=Random.rangeFloat(this._worldMin.x,this._worldMax.x);
+		let y=Random.rangeFloat(this._worldMin.y,this._worldMax.y);
+		this.node.setPosition(x,y);
+	}
+	
+	public setPositionToTop(isResetVelocityY:boolean,resetVelocityY_Value:number):void{
+		let pos=this.node.getPosition();
+		pos.y=this._worldMax.y+this.node.getBoundingBox().height*0.5;
+		this.node.setPosition(pos);
+		
+		if(isResetVelocityY){
+			this._velocity.y=resetVelocityY_Value;
+		}
+	}
+	
+}
