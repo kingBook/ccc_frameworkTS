3d98ce2252d431b5f545d6733171882316c5f11f
kingBook <412126604@qq.com>
2020-08-02 15:32:59 +0800

    update 2020-08-02 15:32

--- packages/flash-tools/main.js
+++ packages/flash-tools/main.js
@@ -21,8 +21,13 @@ module.exports={
 			module.exports.queryAndCreateAnim();
 		},
 		//执行菜单'Tools/Create anim with config'时调用
-		'flash-tools:menu-create-anim'(){
+		'flash-tools:menu-create-anim-with-config'(){
 			module.exports.queryAndCreateAnim();
+		},
+		//执行菜单'Tools/Create anim with images'时调用
+		'flash-tools:menu-create-anim-with-images'(){
+			let selections=Editor.Selection.curSelection("asset");//当前选中资源的uuid列表
+			module.exports.createAnimWithImages(selections);
 		},
 		//执行菜单'Tools/Create node with .anim'时调用
 		'flash-tools:menu-create-node-with-anim'(){
@@ -32,8 +37,85 @@ module.exports={
 	},//end messages
 	
 	/**
+	 * 根据当前选中项创建动画
+	 * @param {string[]} selections 当前选中资源的uuid列表（并不一定全是texture）
+	 */
+	createAnimWithImages(selections){
+		let imageDatas=[];
+		for(let i=0,len=selections.length;i<len;i++){
+			let assetUuid=selections[i];
+			let assetInfo=Editor.assetdb.assetInfoByUuid(assetUuid);//info.path,info.url,info.type,info.isSubAsset
+			if(assetInfo.type==="texture"){
+				let assetName=assetInfo.url.substring(assetInfo.url.lastIndexOf('/')+1);
+				let dotIndex=assetName.lastIndexOf('.');
+				if(dotIndex>-1)assetName=assetName.substring(0,dotIndex);//去除后缀名
+				
+				let assetNumString=assetName.match(/\d+/);
+				let noNumAssetName=assetName.replace(assetNumString,'');
+				
+				if(Editor.assetdb.containsSubAssetsByUuid(assetUuid)){
+					let subAssetInfos=Editor.assetdb.subAssetInfosByUuid(assetUuid);
+					if(subAssetInfos.length==1){
+						let subAssetInfo=subAssetInfos[0];
+						if(subAssetInfo.type==="sprite-frame"){
+							//修改trimType为None
+							let assetMetaStrings=fs.readFileSync(assetInfo.path+".meta","utf8");
+							let assetMetaData=JSON.parse(assetMetaStrings);
+							for(let key in assetMetaData.subMetas){
+								let spriteFrameMetaData=assetMetaData.subMetas[key];
+								spriteFrameMetaData.trimType="none";
+								spriteFrameMetaData.offsetX=0;
+								spriteFrameMetaData.offsetY=0;
+								spriteFrameMetaData.trimX=0;
+								spriteFrameMetaData.trimY=0;
+								spriteFrameMetaData.width=spriteFrameMetaData.rawWidth;
+								spriteFrameMetaData.height=spriteFrameMetaData.rawHeight;
+								spriteFrameMetaData.borderTop=0;
+								spriteFrameMetaData.borderBottom=0;
+								spriteFrameMetaData.borderLeft=0;
+								spriteFrameMetaData.borderRight=0;
+								break;//只设置第一个，因为只有一个spriteFrame
+							}
+							let assetMetaDataStrings=JSON.stringify(assetMetaData,null,"\t");
+							Editor.assetdb.saveMeta(assetInfo.uuid,assetMetaDataStrings,(err,meta)=>{});
+							
+							imageDatas.push({
+								spriteFrameUuid:subAssetInfo.uuid,
+								assetName:assetName, 
+								noNumAssetName:noNumAssetName, 
+								assetUrl:assetInfo.url,
+								num:parseInt(assetNumString) 
+							});
+						}
+					}
+				}
+			}
+		}
+		
+		if(imageDatas.length>0){
+			if(!isSingleFrameCreateAnim && imageDatas.length==1)return;
+			
+			imageDatas.sort((a,b)=>a.num-b.num);
+			
+			let frameDatas=[];
+			for(let i=0,len=imageDatas.length;i<len;i++){
+				frameDatas[i]={spriteFrameUuid:imageDatas[i].spriteFrameUuid};
+			}
+		
+			let zeroNoNumName=imageDatas[0].noNumAssetName;
+			let zeroUrl=imageDatas[0].assetUrl;
+			
+			let animUrl=zeroUrl.substr(0,zeroUrl.lastIndexOf('/')+1);
+			animUrl+=zeroNoNumName+".anim";
+			module.exports.createSequenceFrameAnimationClip(zeroNoNumName,frameDatas,animUrl)
+		}
+	},
+	
+	
+	
+	/**
 	 * 根据当前选中项创建动画节点
-	 * @param {string[]} selections 当前选中资源的uuid列表（并不一定是.anim文件）
+	 * @param {string[]} selections 当前选中资源的uuid列表（并不一定全是.anim）
 	 */
 	createAnimNodeWithSelections(selections){
 		for(let i=0,len=selections.length;i<len;i++){
@@ -48,7 +130,10 @@ module.exports={
 	/**
 	 * 根据.anim文件的uuid和信息创建动画节点
 	 * @param {string} animFileUuid .anim文件的uuid
-	 * @param {any} info .anim文件信息 path,url,type,isSubAsset
+	 * @param {any} info .anim文件信息 
+	 * ```
+	 * { path:string, url:string, type:string, isSubAsset:boolean }
+	 * ```
 	 */
 	createAnimNodeWithUuid(animFileUuid,info){
 		let animStrings=fs.readFileSync(info.path,"utf8");
@@ -470,6 +555,70 @@ module.exports={
 		}
 		return animData;
 	},
+	
+	/**
+	 * 创建序列帧动画
+	 * @param {string} animFileName 创建后的.anim文件的名称（不包含后缀名）
+	 * @param {any[]} frameDatas 帧数据列表，元素格式：
+	 * ```
+	 * { spriteFrameUuid:xxxx-xxxx-xxxx-xxxx-xxxx, anchor:{x:0.5,y:0.5} }
+	 * ```
+	 * 当anchor为undefined时，忽略anchor
+	 * @param {string} animUrl 如："db://assets/textures/xx.anim"
+	 */
+	createSequenceFrameAnimationClip(animFileName,frameDatas,animUrl){
+		let frameCount=frameDatas.length;
+		
+		let animData={
+			"__type__":"cc.AnimationClip",
+			"_name":animFileName,
+			"_objFlags":0,
+			"_native":"",
+			"_duration":0,
+			"sample":24,
+			"speed":1,
+			"wrapMode":2,
+			"curveData":{
+				"comps":{
+					"cc.Sprite":{
+						"spriteFrame":[]
+					}
+				}
+			},
+			"events":[]
+		};
+		
+		let interval=1/animData.sample;
+		animData._duration=interval*frameCount;
+		
+		let spriteFrameList=animData["curveData"]["comps"]["cc.Sprite"]["spriteFrame"];
+		
+		for(let i=0;i<frameCount;i++){
+			let frameData=frameDatas[i];
+			let frameTime=interval*i;
+			//spriteFrame
+			spriteFrameList[i]={
+				"frame":frameTime,
+				"value":{
+					"__uuid__":frameData.spriteFrameUuid
+				}
+			};
+			//anchor
+			if(frameData.anchor){
+				if(!animData.curveData["props"]){
+					animData.curveData["props"]={
+						"anchorX":[],
+						"anchorY":[]
+					};
+				}
+				animData.curveData["props"]["anchorX"][i]={frame:frameTime, value:frameData.anchor.x, curve:"constant"};
+				animData.curveData["props"]["anchorY"][i]={frame:frameTime, value:frameData.anchor.y, curve:"constant"};
+			}
+		}
+		let animDataStrings=JSON.stringify(animData,null,"\t");
+		module.exports.createAnimationClipFile(animUrl,animDataStrings);
+	},
+	
 	
 	/**
 	 * 创建.anim文件
--- packages/flash-tools/scene-walker.js
+++ packages/flash-tools/scene-walker.js
@@ -4,9 +4,15 @@ module.exports={
 		let scene=cc.director.getScene();
 		let childrenCount=scene.childrenCount;
 		let lastNode=scene.children[childrenCount-1];
-		
-		lastNode.anchorX=parseFloat(animData.curveData.props.anchorX[0].value);
-		lastNode.anchorY=parseFloat(animData.curveData.props.anchorY[0].value);
+		//设置node的anchor与第一帧相同
+		if(animData.curveData && animData.curveData.props){
+			if(animData.curveData.props.anchorX){
+				lastNode.anchorX=parseFloat(animData.curveData.props.anchorX[0].value);
+			}
+			if(animData.curveData.props.anchorY){
+				lastNode.anchorY=parseFloat(animData.curveData.props.anchorY[0].value);
+			}
+		}
 		
 		let sprite=lastNode.addComponent(cc.Sprite);
 		let animation=lastNode.addComponent(cc.Animation);
