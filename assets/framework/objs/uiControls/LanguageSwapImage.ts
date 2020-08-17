ec290a4e411b7b15c4665a63f01f99910cadbd02
kingBook <412126604@qq.com>
2020-08-17 17:58:29 +0800

    update 2020-08-17 17:58

---
+++ assets/framework/objs/uiControls/LanguageSwapImage.ts
@@ -0,0 +1,55 @@
+﻿import BaseBehaviour from "../../core/BaseBehaviour";
+import App, { Language } from "../../core/App";
+import SwapSpriteFrame from "./SwapSpriteFrame";
+/**
+ * 根据语言自动交换图片
+ */
+const {ccclass,property}=cc._decorator;
+@ccclass
+export default class LanguageSwapImage extends BaseBehaviour{
+	@property({type:cc.Sprite,visible:true})
+	private _sprite:cc.Sprite=null;
+	@property({type:cc.SpriteFrame,visible:true})
+	private _en:cc.SpriteFrame=null;
+	@property({type:cc.SpriteFrame,visible:true})
+	private _cn:cc.SpriteFrame=null;
+	
+	protected resetInEditor():void{
+		super.resetInEditor();
+		if(this._sprite===null){
+			let tempSprite=this.getComponent(cc.Sprite);
+			if(tempSprite){
+				this._sprite=tempSprite;
+			}else{
+				let button=this.getComponent(cc.Button);
+				if(button && button.target){
+					this._sprite=button.target.getComponent(cc.Sprite);
+				}
+			}
+		}
+	}
+	
+	protected onLoad():void{
+		super.onLoad();
+	}
+	
+	protected start():void{
+		super.start();
+		this.swapToLanguage(App.instance.language);
+		App.instance.node.on(App.CHANGE_LANGUAGE,this.onChangeLanguage,this);
+	}
+	
+	private onChangeLanguage(language:Language):void{
+		this.swapToLanguage(language);
+	}
+	
+	private swapToLanguage(language:Language):void{
+		this._sprite.spriteFrame=(language===Language.EN)?this._en:this._cn;
+	}
+	
+	protected onDestroy():void{
+		App.instance.node.off(App.CHANGE_LANGUAGE,this.onChangeLanguage,this);
+		super.onDestroy();
+	}
+	
+}
--- assets/framework/objs/uiControls/SwapSpriteFrame.ts
+++ assets/framework/objs/uiControls/SwapSpriteFrame.ts
@@ -5,18 +5,25 @@ const{ccclass,property}=cc._decorator;
 /** 交换Sprite组件的SpriteFrame */
 @ccclass
 export default class SwapSpriteFrame extends BaseBehaviour{
-    @property({type:cc.Sprite,visible:true,tooltip:"用于切换SpriteFrame的Sprite组件,null时自动从当前节点获取"})
+    @property({type:cc.Sprite,visible:true,tooltip:"用于切换SpriteFrame的Sprite组件"})
 	private _sprite:cc.Sprite=null;
     @property({type:[cc.SpriteFrame],visible:true,tooltip:"切换的SpriteFrame列表，长度为:2"})
 	private _spriteFrames:cc.SpriteFrame[]=[null,null];
 	
-	protected onLoad():void{
-		super.onLoad();
-		//
-		if(this._sprite==null){
-			this._sprite=this.getComponent(cc.Sprite);
+	protected resetInEditor():void{
+		super.resetInEditor();
+		if(this._sprite===null){
+			let tempSprite=this.getComponent(cc.Sprite);
+			if(tempSprite){
+				this._sprite=tempSprite;
+			}else{
+				let button=this.getComponent(cc.Button);
+				if(button && button.target){
+					this._sprite=button.target.getComponent(cc.Sprite);
+				}
+			}
 		}
-    }
+	}
     
     public autoSwap():void{
 		if(this._sprite.spriteFrame==this._spriteFrames[0]){
