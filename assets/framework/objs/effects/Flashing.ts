5e4a6b035215f51eec14e646a9dc0fc0862780cd
kingBook <412126604@qq.com>
2020-08-31 17:53:45 +0800

    update 2020-08-31 17:53

--- assets/framework/objs/effects/Flashing.ts
+++ assets/framework/objs/effects/Flashing.ts
@@ -15,6 +15,10 @@ export default class Flashing extends BaseBehaviour{
 	
 	public get isFlashing():boolean{ return this._isFlashing; }
 	
+	/**
+	 * 开始闪烁
+	 * @param time 闪烁的时间<秒>
+	 */
 	public startFlashing(time:number=3):void{
 		if(this._isFlashing)return;
 		this._isFlashing=true;
@@ -28,6 +32,20 @@ export default class Flashing extends BaseBehaviour{
 			this.onComplete();
 		})
 		.start();
+	}
+	
+	/**
+	 * 结束闪烁
+	 * @param isEmitComppleteEvent 是否发送闪烁完成事件
+	 */
+	public endFlashing(isEmitComppleteEvent:boolean=false):void{
+		if(this._isFlashing){
+			this._isFlashing=false;
+			this.node.opacity=this._opacityRecord;
+			if(isEmitComppleteEvent){
+				this.node.emit(Flashing.COMPLETE);
+			}
+		}
 	}
 	
 	private onComplete():void{
